814c325c1925f19a15349fa05323e5bde48d597d
Vladimir Ivashkevich <v.v.ivashkevich@gmail.com>
2023-09-23 19:13:55 +0800

    add django/4.DRF/1.smart_home

---
+++ django/4.DRF/1.smart_home/measurement/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.DecimalField(max_digits=3, decimal_places=1)
+    image = models.ImageField(blank=True)
+    created_at = models.DateTimeField(auto_now=True)
